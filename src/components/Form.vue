53c0d37f56e2c7dc6800107875add1cc854a8bc5
Chris Lane <pankokrumbs@gmail.com>
2020-07-28 16:19:38 -0600

    Create inputs and text area for form

---
+++ src/components/Form.vue
@@ -0,0 +1,23 @@
+<template>
+  <div >
+    <label>Title</label>
+    <input type="text" id="recipe-title">
+    <label>Instructions</label>
+    <textarea></textarea>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

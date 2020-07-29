2557b0a4eb1f420eac9034f9f1acf1b6c7006ffb
Chris Lane <pankokrumbs@gmail.com>
2020-07-28 22:38:24 -0600

    All functionality

--- src/components/BrowseRecipes.vue
+++ src/components/BrowseRecipes.vue
@@ -1,22 +1,39 @@
 <template>
-  <div>
+  <div v-if="showCard === false">
     <p v-if="cards.length < 1">You have no recipes to browse please create one</p>
-    <div v-for="card in cards" v-bind:key="card">
-      <article>
-        <h1>{{card.title}}</h1>
-        <p>{{card.ingredients}}</p>
-        <p>{{card.instructions}}</p>
-        <p>{{card.rating}}</p>
-      </article>
+    <div v-for="(card,i) in cards" v-bind:key="card">
+      <h1 v-on:click="toggleShowCard(i)">{{card.title}}</h1>
     </div>
+  </div>
+  <div v-else>
+    <RecipeCard @clear-card="toggleShowCard" v-bind:card="cards[cardIndex]"></RecipeCard>
+    <!-- <h1>{{cards[cardIndex]}}</h1> -->
+    <!-- <p>{{card[cardIndex][ingredients]}}</p>
+    <p>{{card[cardIndex][instructions]}}</p>
+    <p>{{card[cardIndex][rating]}}</p> -->
   </div>
 </template>
 <script>
+import RecipeCard from './RecipeCard.vue'
+
 export default {
   name: "BrowseRecipes",
-  props: ['cards'],
+  data() {
+    return {
+      showCard: false,
+      cardIndex: null
+    };
+  },
+  methods: {
+    toggleShowCard(i) {
+      console.log(i);
+      this.showCard = !this.showCard;
+      this.cardIndex = i;
+    }
+  },
+  props: ["cards"],
   components: {
-    // RecipeCard
+    RecipeCard
   }
 };
 </script>
---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,24 @@
+<template>
+<article>
+  <h1>{{card.title}}</h1>
+  <p>{{card.ingredients}}</p>
+  <p>{{card.instructions}}</p>
+  <p>{{card.rating}}</p>
+  <button v-on:click="clearCard">x</button>
+</article>
+</template>
+<script>
+export default {
+  name: 'RecipeCard',
+  props: ["card"],
+  methods: {
+    clearCard(){
+      
+      this.$emit("clear-card")
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
